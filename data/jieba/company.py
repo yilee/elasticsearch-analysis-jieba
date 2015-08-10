a6ea7aa5a2062ce2328bfb50509ea627ebb5a9f0
kshi <kshi@linkedin.com>
2015-08-10 11:06:51 +0800

    add new dict

--- data/jieba/company.py
+++ data/jieba/company.py
@@ -1,8 +1,9 @@
-filename = 'industry.dic'
-filename2 = 'industry.dict'
+filename = 'university.dict'
+filename2 = 'university_2.dict'
 
 f = open(filename2, 'w')
 for l in open(filename).readlines():
 	l = l.strip()
+	l = l.replace(' ','')
 	f.write(l + ' 3\n')
 f.close()
