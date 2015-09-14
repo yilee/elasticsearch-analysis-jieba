2bebea67d413d07bd54f352eee4b418f60939b9f
kshi <kshi@linkedin.com>
2015-09-14 10:15:32 +0800

    add JiebaTokenizer

--- src/main/java/org/elasticsearch/index/analysis/JiebaTokenizer.java
+++ src/main/java/org/elasticsearch/index/analysis/JiebaTokenizer.java
@@ -33,17 +33,13 @@ public class JiebaTokenizer extends Tokenizer {
     private Iterator<SegToken> tokenIter;
     private List<SegToken> array;
 
-
     //词元文本属性
     private final CharTermAttribute termAtt;
     //词元位移属性
     private final OffsetAttribute offsetAtt;
-    //词元分类属性（该属性分类参考org.wltea.analyzer.core.Lexeme中的分类常量）
+    //词元分类属性
     private final TypeAttribute typeAtt;
-    //记录最后一个词元的结束位置
-    private int endPosition;
 
-    private int skippedPositions;
     private String type;
 
     private PositionIncrementAttribute posIncrAtt;
@@ -60,7 +56,7 @@ public class JiebaTokenizer extends Tokenizer {
 
     @Override
     public boolean incrementToken() throws IOException {
-        if(input == null){
+        if (input == null) {
             return false;
         }
 
@@ -72,7 +68,6 @@ public class JiebaTokenizer extends Tokenizer {
         }
         clearAttributes();
         SegToken token = tokenIter.next();
-        // System.out.println("token:" + token);
         offsetAtt.setOffset(token.startOffset, token.endOffset);
         String tokenString = token.word;
         termAtt.copyBuffer(tokenString.toCharArray(), 0, tokenString.length());
